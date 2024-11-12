9305a30953348c2a6513087b1888eaae745ac9f2
xMarioOP <marioalejandro424@outlook.com>
2024-11-12 13:20:47 -0500

    using async-await

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
+const newImageBtn = document.getElementById("btn")
+
 const URL = "https://api.thecatapi.com/v1/images/search"
 
-fetch(URL)
-    .then(res => res.json())
-    .then(data => {
+
+async function fetchData(url) {
+    try {
+        const response = await fetch(url)
+        if (!response.ok) {
+            throw new Error("Error")
+        }
+        const data = await response.json()
+
         img = document.querySelector("img")
         img.src = data[0].url
+        console.log(data[0].url)
 
+    } catch (error) {
+        console.error("error", error);
+    }
+}
 
-        console.log(data[0].url);
-    });
+newImageBtn.addEventListener("click", () => {
+    fetchData(URL)
+})
+fetchData(URL)
+
