9450c107d44aeb65b09b44da46722e861714661e
xMarioOP <marioalejandro424@outlook.com>
2024-11-12 12:41:33 -0500

    Setup

---
+++ main.js
@@ -0,0 +1,11 @@
+const URL = "https://api.thecatapi.com/v1/images/search"
+
+fetch(URL)
+    .then(res => res.json())
+    .then(data => {
+        img = document.querySelector("img")
+        img.src = data[0].url
+
+
+        console.log(data[0].url);
+    })
