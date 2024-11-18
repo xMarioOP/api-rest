afbfb5329142c31324cf9ccc4a08eeee299f0c87
xMarioOP <marioalejandro424@outlook.com>
2024-11-18 15:43:02 -0500

    Fix and enhance saveFavoritesCats functionality

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const img3 = document.getElementById("img3")
 
 const spanError = document.getElementById("error")
 
+const saveFavoritesCat = document.querySelectorAll(".saveFavoriteCat")
+
 
 const API_URL_RANDOM = "https://api.thecatapi.com/v1/images/search?limit=2&api_key=live_IXdbF6NTpXDU8aDS8LQPdlyR2xkm0IMGq9Oo8R0X403cKFR0VlI2ZG8JOaZqFbIG"
 
@@ -49,6 +51,34 @@ async function loadFavoritesCats(url) {
         spanError.innerHTML = `${error.message}`
     }
 }
+
+// SAVE FAVORITES CATS
+async function saveFavoritesCats(url) {
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                image_id: "99v"
+            })
+        })
+        const data = await response.json()
+        if (!response.ok) {
+            throw new Error(`Error ${response.status}: ${data.message || "unknown error"}`)
+        }
+    } catch (error) {
+        console.error("error", error)
+        spanError.innerHTML = `${error.message}`
+    }
+}
+
+saveFavoritesCat.forEach(button => {
+    button.addEventListener("click", () => {
+        saveFavoritesCats(API_URL_FAVORITES);
+    });
+});
 
 // CALL FOR FUNCTIONS
 loadRandomCats(API_URL_RANDOM)
