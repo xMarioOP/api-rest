d98ece202969ac6c6da31221c2e580204597290b
xMarioOP <marioalejandro424@outlook.com>
2024-11-18 18:37:53 -0500

    Add functions to delete and reload DOM for favorite cats

--- main.js
+++ main.js
@@ -10,8 +10,10 @@ const btn2 = document.getElementById("btn2")
 
 
 const API_URL_RANDOM = "https://api.thecatapi.com/v1/images/search?limit=5&api_key=live_IXdbF6NTpXDU8aDS8LQPdlyR2xkm0IMGq9Oo8R0X403cKFR0VlI2ZG8JOaZqFbIG"
-
 const API_URL_FAVORITES = "https://api.thecatapi.com/v1/favourites?limit=5&api_key=live_IXdbF6NTpXDU8aDS8LQPdlyR2xkm0IMGq9Oo8R0X403cKFR0VlI2ZG8JOaZqFbIG"
+const API_URL_FAVORITES_DELETE = (id) => `https://api.thecatapi.com/v1/favourites/${id}?limit=5&api_key=live_IXdbF6NTpXDU8aDS8LQPdlyR2xkm0IMGq9Oo8R0X403cKFR0VlI2ZG8JOaZqFbIG`
+
+
 
 
 // RANDOM CATS
@@ -43,7 +45,7 @@ newImageBtn.addEventListener("click", () => {
 })
 
 
-// FAVORITE CATS
+// LOAD FAVORITE CATS
 async function loadFavoritesCat(url) {
     try {
         const response = await fetch(url)
@@ -51,15 +53,26 @@ async function loadFavoritesCat(url) {
         if (!response.ok) {
             throw new Error(`Error ${response.status}: ${data.message || "unknown error"}`)
         }
+        const section = document.getElementById("favorites-cats")
+        section.innerHTML = ""
+        const h2 = document.createElement("h2")
+        const h2Text = document.createTextNode("favorite Cats")
+        h2.appendChild(h2Text)
+        section.appendChild(h2)
+
         console.log("FAVORITE:", data)
         data.forEach(cat => {
-            const section = document.getElementById("favorites-cats")
+            console.log(cat.id);
+
             const article = document.createElement("article")
             const image = document.createElement("img")
             const button = document.createElement("button")
             const buttonText = document.createTextNode("Remove cat of favorites")
-
             button.appendChild(buttonText)
+            button.addEventListener("click", () => {
+                deleteFavoritesCats(cat.id)
+            })
+
             image.src = cat.image.url
             image.width = 180
 
@@ -76,7 +89,7 @@ async function loadFavoritesCat(url) {
 
 // SAVE FAVORITES CATS
 async function saveFavoritesCats(id) {
-    const API_URL_FAVORITES = "https://api.thecatapi.com/v1/favourites?api_key=live_IXdbF6NTpXDU8aDS8LQPdlyR2xkm0IMGq9Oo8R0X403cKFR0VlI2ZG8JOaZqFbIG"; // URL fija
+    const API_URL_FAVORITES = "https://api.thecatapi.com/v1/favourites?api_key=live_IXdbF6NTpXDU8aDS8LQPdlyR2xkm0IMGq9Oo8R0X403cKFR0VlI2ZG8JOaZqFbIG";
     try {
         const response = await fetch(API_URL_FAVORITES, {
             method: "POST",
@@ -89,13 +102,14 @@ async function saveFavoritesCats(id) {
         });
         console.log(id);
 
-        // Verificar si la respuesta es correcta
+
         if (!response.ok) {
             throw new Error(`Error ${response.status}: ${response.statusText}`);
         }
 
         const data = await response.json();
         console.log("Favorite cat saved:", data);
+        loadFavoritesCat(API_URL_FAVORITES)
     } catch (error) {
         console.error("Error saving favorite cat:", error);
         spanError.innerHTML = `${error.message}`;
@@ -103,47 +117,28 @@ async function saveFavoritesCats(id) {
 }
 
 
+// DELETE FAVORITES CATS
+async function deleteFavoritesCats(id) {
+    try {
+        const response = await fetch(API_URL_FAVORITES_DELETE(id), { method: "DELETE" });
 
-// CALL FOR FUNCTIONS
+        if (!response.ok) {
+            throw new Error(`Error ${response.status}: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        console.log("Favorite cat saved:", data);
+        loadFavoritesCat(API_URL_FAVORITES)
+    } catch (error) {
+        console.error("Error saving favorite cat:", error);
+        spanError.innerHTML = `${error.message}`;
+    }
+}
+
+
+// INITIALIZE FUNCTIONS
 loadRandomCats(API_URL_RANDOM)
 loadFavoritesCat(API_URL_FAVORITES)
 
 
 
-
-//////////////ELIMINAR TODOS LOS FAVORITOS/////////////////////////
-// document.getElementById("clear-favorites-btn").addEventListener("click", clearFavorites);
-
-// // Eliminar todos los gatos de los favoritos
-// async function clearFavorites() {
-//     const API_URL_FAVORITES = "https://api.thecatapi.com/v1/favourites?api_key=live_IXdbF6NTpXDU8aDS8LQPdlyR2xkm0IMGq9Oo8R0X403cKFR0VlI2ZG8JOaZqFbIG"; // URL fija
-
-//     try {
-//         // Obtener los favoritos (todos)
-//         const response = await fetch(API_URL_FAVORITES);
-//         const data = await response.json();
-
-//         // Verificar que la respuesta sea correcta
-//         if (!response.ok) {
-//             throw new Error(`Error ${response.status}: ${data.message || "unknown error"}`);
-//         }
-
-//         // Eliminar todos los gatos de los favoritos
-//         const deletePromises = data.map(cat => {
-//             return fetch(`https://api.thecatapi.com/v1/favourites/${cat.id}?api_key=live_IXdbF6NTpXDU8aDS8LQPdlyR2xkm0IMGq9Oo8R0X403cKFR0VlI2ZG8JOaZqFbIG`, {
-//                 method: "DELETE"
-//             });
-//         });
-
-//         // Esperar a que todas las eliminaciones se completen
-//         await Promise.all(deletePromises);
-//         console.log("All favorite cats removed.");
-
-//         // Recargar la lista de favoritos después de eliminar todos
-//         loadFavoritesCat(API_URL_FAVORITES);
-
-//     } catch (error) {
-//         console.error("Error removing favorites:", error);
-//         spanError.innerHTML = `${error.message}`;
-//     }
-// }
